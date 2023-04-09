cfe3a3a4b4d5632c6770c9c42eb42c7a51ffcd8f
yang,zhong <yang-zhong@udious.com>
2023-04-09 21:37:10 +0800

    new implementation

--- logf.go
+++ logf.go
@@ -1,17 +1,62 @@
 package logf
 
-import "go-micro.dev/v4/logger"
+import (
+	"fmt"
+	"log"
+)
 
-type Logfer interface {
-	Logf(level logger.Level, format string, v ...interface{})
+type Level uint8
+
+const (
+	Trace Level = iota
+	Debug
+	Info
+	Warn
+	Error
+	Fatal
+)
+
+var levelString map[Level]string
+
+func init() {
+	levelString = map[Level]string{
+		Trace: "TRACE",
+		Debug: "DEBUG",
+		Info:  "INFO",
+		Warn:  "WARN",
+		Error: "ERROR",
+		Fatal: "FATAL",
+	}
 }
 
-type Logf func(level logger.Level, format string, v ...interface{})
+type Logfer interface {
+	Logf(level Level, format string, v ...interface{})
+}
 
-func (logf Logf) Logf(level logger.Level, format string, v ...interface{}) {
+type Logf func(level Level, format string, v ...interface{})
+
+func (logf Logf) Logf(level Level, format string, v ...interface{}) {
 	logf(level, format, v...)
 }
 
-func New() Logfer {
-	return logger.NewLogger()
+type logger struct {
+	underling *log.Logger
+}
+
+func New(loggers ...*log.Logger) Logfer {
+	if len(loggers) > 0 {
+		return logger{underling: loggers[0]}
+	}
+	return logger{underling: log.Default()}
+}
+
+func (l logger) Logf(level Level, format string, v ...any) {
+	l.underling.Printf("%s  %s", LevelString(level), fmt.Sprintf(format, v...))
+}
+
+func LevelString(level Level) string {
+	if s, ok := levelString[level]; ok {
+		return s
+	}
+	return "UNKOWN"
 }
