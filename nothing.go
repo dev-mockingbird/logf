a20276ea593957f110051bd5c734c54e30bc5b20
Oliver Yang <oliverzyang@tencent.com>
2023-06-20 14:02:26 +0800

    add nothing logger

---
+++ nothing.go
@@ -0,0 +1,15 @@
+package logf
+
+type nothing struct{}
+
+func Nothing() Logger {
+	return &nothing{}
+}
+
+func (n *nothing) Prefix(string) Logger {
+	return n
+}
+
+func (n *nothing) Logf(Level, string, ...any) {
+
+}
